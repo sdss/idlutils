d4bc5e6c8dd0eaa5de5c3bc3f4a3f0ac3bef4bb0
schlegel <schlegel@AlbireoAir.local>
2003-09-19 04:08:20 +0000

    Fix that I was counting bytes incorrectly.  Just treat the array as byte values in this routine.

--- src/math/memshift.c
+++ src/math/memshift.c
@@ -9,14 +9,14 @@ IDL_LONG memshift
   (int      argc,
    void *   argv[])
 {
-   IDL_LONG *  array;
+   unsigned char *  array;
    IDL_LONG    isrc;
    IDL_LONG    idest;
    IDL_LONG    nbyte;
    IDL_LONG    retval = 1;
 
    /* Allocate pointers from IDL */
-   array = (IDL_LONG *)argv[0];
+   array = (unsigned char *)argv[0];
    isrc = *((IDL_LONG *)argv[1]);
    idest = *((IDL_LONG *)argv[2]);
    nbyte = *((IDL_LONG *)argv[3]);
