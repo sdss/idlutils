1f42d618b50a4527fa1ffac031a0dd0b26f04842
schlegel <schlegel@AlbireoAir.local>
2006-03-28 05:53:40 +0000

    Add two more possible C calls for POPULATE_IMAGE to pass X,Y as integer values rather than floats.  This fixes a problem whereby if the image had more than about 2^24 elements, one could populate that incorrectly due to round-off errors in the conversion of X,Y from ints to floats. This is in response to PR #6946 from Finkbeiner.

--- src/image/pop_image.c
+++ src/image/pop_image.c
@@ -4,7 +4,7 @@
 #include "export.h"
 
 /******************************************************************************/
-IDL_LONG pop_image
+IDL_LONG pop_image_float_float
   (int         argc,
    void    *   argv[])
 {
@@ -70,9 +70,55 @@ IDL_LONG pop_image
 
    return retval;
 }
+/******************************************************************************/
+IDL_LONG pop_image_float_long
+  (int         argc,
+   void    *   argv[])
+{
+   IDL_LONG    npts;
+   IDL_LONG *  xvec;
+   IDL_LONG *  yvec;
+   float    *  wvec;
+   IDL_LONG    nx;
+   IDL_LONG    ny;
+   float    *  image;
+   IDL_LONG    iassign;
+
+   IDL_LONG    ipt;
+   IDL_LONG    ix1;
+   IDL_LONG    iy1;
+   IDL_LONG    retval = 1;
+   float       dx;
+   float       dy;
+
+   /* Allocate pointers from IDL */
+   npts = *((IDL_LONG *)argv[0]);
+   xvec = (IDL_LONG *)argv[1];
+   yvec = (IDL_LONG *)argv[2];
+   wvec = (float *)argv[3];
+   nx = *((IDL_LONG *)argv[4]);
+   ny = *((IDL_LONG *)argv[5]);
+   image = (float *)argv[6];
+   iassign = *((IDL_LONG *)argv[7]);
+
+   if (iassign == 0 || iassign == 1) {
+
+      /* NGP or CIC assignment */
+
+      for (ipt=0; ipt < npts; ipt++) {
+         ix1 = xvec[ipt];
+         iy1 = yvec[ipt];
+
+         if (ix1 >= 0 && ix1 < nx && iy1 >= 0 && iy1 < ny)
+          image[ix1+iy1*nx] += wvec[ipt];
+      }
+   }
+
+   return retval;
+}
 
 /******************************************************************************/
-IDL_LONG pop_image_double
+IDL_LONG pop_image_double_double
   (int         argc,
    void    *   argv[])
 {
@@ -134,6 +180,52 @@ IDL_LONG pop_image_double
           image[ix1+1+(iy1+1)*nx] += wvec[ipt] * (1.0 - dx) * (1.0 - dy);
       }
 
+   }
+
+   return retval;
+}
+/******************************************************************************/
+IDL_LONG pop_image_double_long
+  (int         argc,
+   void    *   argv[])
+{
+   IDL_LONG    npts;
+   IDL_LONG *  xvec;
+   IDL_LONG *  yvec;
+   double   *  wvec;
+   IDL_LONG    nx;
+   IDL_LONG    ny;
+   double   *  image;
+   IDL_LONG    iassign;
+
+   IDL_LONG    ipt;
+   IDL_LONG    ix1;
+   IDL_LONG    iy1;
+   IDL_LONG    retval = 1;
+   double      dx;
+   double      dy;
+
+   /* Allocate pointers from IDL */
+   npts = *((IDL_LONG *)argv[0]);
+   xvec = (IDL_LONG *)argv[1];
+   yvec = (IDL_LONG *)argv[2];
+   wvec = (double *)argv[3];
+   nx = *((IDL_LONG *)argv[4]);
+   ny = *((IDL_LONG *)argv[5]);
+   image = (double *)argv[6];
+   iassign = *((IDL_LONG *)argv[7]);
+
+   if (iassign == 0 || iassign == 1) {
+
+      /* NGP or CIC assignment */
+
+      for (ipt=0; ipt < npts; ipt++) {
+         ix1 = xvec[ipt];
+         iy1 = yvec[ipt];
+
+         if (ix1 >= 0 && ix1 < nx && iy1 >= 0 && iy1 < ny)
+          image[ix1+iy1*nx] += wvec[ipt];
+      }
    }
 
    return retval;
